a6674792031f7cabaa4dc6f30855b52d8e845311
iraqlaptop <majdahmd418@gmail.com>
2017-12-28 01:54:48 +0300

    aaaaaaaa

--- src/mathematical/Mats.java
+++ src/mathematical/Mats.java
@@ -149,7 +149,7 @@ public class Mats extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
+        // TODO add your handling code:
         Sums s = new Sums();
         s.set("Sum");
         s.setResizable(false);
